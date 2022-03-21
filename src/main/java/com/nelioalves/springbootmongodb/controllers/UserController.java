91107361ef1a529e52a1eb0c020366f335c93a38
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-03-21 16:03:09 -0300

    User, UserController

---
+++ src/main/java/com/nelioalves/springbootmongodb/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.nelioalves.springbootmongodb.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	
+	
+
+}
