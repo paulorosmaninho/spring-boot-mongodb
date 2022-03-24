19f4c7cc26d186b03d2810d1fe47b33e6c6d5ea9
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-03-24 15:28:05 -0300

    Projeção dos dados do author com DTO

--- src/main/java/com/nelioalves/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/nelioalves/springbootmongodb/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.nelioalves.springbootmongodb.dto.AuthorDTO;
 import com.nelioalves.springbootmongodb.entities.Post;
 import com.nelioalves.springbootmongodb.entities.User;
 import com.nelioalves.springbootmongodb.repositories.PostRepository;
@@ -42,8 +43,8 @@ public class Instantiation implements CommandLineRunner{
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 			
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
